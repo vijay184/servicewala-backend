5449ec1326ae9d786b8ac2daf01935577475e752
vijay184 <116822236+vijay184@users.noreply.github.com>
2023-07-26 13:17:28 +0530

    Update functions.js

--- utils/functions.js
+++ utils/functions.js
@@ -11,7 +11,6 @@ export const calculateDistance = (lat1, lon1, lat2, lon2) => {
       Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     const distance = earthRadius * c;
-    console.log("Distance:" ,distance)
     return distance;
   }
 
