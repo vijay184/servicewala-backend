ff4f7523e5652c8d7b29be044e71a4fe34db2a13
vijay184 <116822236+vijay184@users.noreply.github.com>
2023-07-26 21:43:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders : ['Content-Type, Authorization'],
     credentials: true,
   })
 );
